95600416c60fe0790acb63ca19bf72cf8142f065
CODEWIFI_SAN <basserue.simon@gmail.com>
2023-05-09 22:13:48 +0200

    YOOOOO

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    token: "MTA0NDE2NDA5NTYyMjAwMDczMA.GFCs67.iqoYoAuiGcu18xJPoUZZZwRCRh1R3CKN-vi2QY"
+}
--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-const Discord = require("discord.js")
-const Player = require("discord-player")
-const intents = new Discord.IntentsBitField(3276799)
-const bot = new Discord.Client({intents})
+import { Client } from "discord.js";
+const bot = new Client({intents: 3276799})
+import { token } from "./config.json";
 
 console.log("Connexion au bot...");
-Client.login("MTA0NDE2NDA5NTYyMjAwMDczMA.G8j9T5.mThEdtzl6gQRcinbKuX5sVHRZWu2cz83hLNTbw")
-    .then(() => console.log("Connecté au bot !"))
-    .catch((error) => console.log("Impossible de se connecter au bot - " + error));
-
-Client.on("ready", async () => {
-
-    await Client.application.commands.set([
-        {
-            name: "ping",
-            description: "Pong!"
-        }
-    ]);
-
-    console.log("Le bot est prêt !");
-});
-
-Client.on("interactionCreate", (interaction) => {
-
-    if (!interaction.isCommand()) return;
-
-    if (interaction.commandName === "ping")
-        interaction.reply("Pong!");
-});
+bot.login(token)
